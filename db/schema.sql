b0562d1bd00e3c3c3b5732b474bca715a99f0b75
jus10 <jus10hollis@users.noreply.github.com>
2022-09-27 11:56:52 -0600

    update including database seeds

--- db/schema.sql
+++ db/schema.sql
@@ -9,9 +9,19 @@ CREATE TABLE roles(
   department_name VARCHAR(100) NOT NULL,
   employee_name INT NOT NULL,
   on_duty BOOLEAN
+  PRIMARY KEY (id)
 );
 
 CREATE TABLE departments(
   id INT NOT NULL,
   department_name VARCHAR(30) NOT NULL
+);
+
+CREATE TABLE staffing_order (
+  id INT,
+  employee_id INT,
+  scheduling_details TEXT,
+  FOREIGN KEY (employee_id)
+  REFERENCES roles(id)
+  ON DELETE SET NULL
 );
---
+++ db/seeds.sql
@@ -0,0 +1,6 @@
+INSERT INTO roles (id, role_name, department_name, employee_name, on_duty)
+VALUES (001, "Manager", "Administration","Alex Suiza", true),
+       (002, "Supervisor", "Logistics","Daphne LaMotta", true),
+       (003, "Technician", "Research","Craig Towns", false),
+       (004, "Associate", "Sales","Andy Fairchild", true),
+       (005, "Assistant", "Sales","Denise Montgomery", true);
